9e4165bee5c56173daeda8a507a0b33e66f6715f
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 08:01:32 -0300

    Fix views folder path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/pessoas', pessoas)
 
-app.set('view',path.join(__dirname, 'views'))
+app.set('views',path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.listen(port, (err) => {
