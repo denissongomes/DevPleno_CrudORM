1ea1c05bfa45875d504df15fcc692fd14e77b588
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 11:51:35 -0300

    Import models dinamicaly not working

--- models/index.js
+++ models/index.js
@@ -4,6 +4,18 @@ const sequelize = new Sequelize('cadastro-orm', 'root','', {
     host: '127.0.0.1'
 } )
 
+// const models = {}
+// const fs = require('fs')
+// const path = require('path')
+ 
+//     fs
+//     .readdirSync(__dirname)
+//     .filter((file) => file !== 'index.js')
+//     .forEach(file => {
+//         const model = require(path.join(__dirname, file)); 
+//         models[models.name] = model(sequelize, Sequelize)
+//         //
+//     });
 const pessoa = require('./pessoas.js')(sequelize, Sequelize); 
 
 module.exports = {
