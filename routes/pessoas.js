fcf796277b19d03e7063cdf1b7ff250168c41618
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 07:14:27 -0300

    Route http get to "/"

--- routes/pessoas.js
+++ routes/pessoas.js
@@ -2,6 +2,6 @@ const express = require('express')
 const pessoasController = require('../controllers/pessoas')
 const router = express.Router()
 
-
+router.get('/', pessoasController.index)
 
 module.exports = router
