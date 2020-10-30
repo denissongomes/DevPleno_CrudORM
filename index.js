8a19f7a62abdab3ad32a08ebbb8b8de8a7dadb5e
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 06:57:07 -0300

    Bind and listen for connections on the host/port

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view',path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log('Não conectado')
+    } else {
+        console.log('Conectado na porta: ' + port)
+    }
+})
