b69104796a3c26fd82adddc073b725e2bc151256
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 09:33:51 -0300

    Import model Pessoas

--- models/index.js
+++ models/index.js
@@ -4,6 +4,6 @@ const sequelize = new Sequelize('cadastro-orm', 'root','', {
     host: '127.0.0.1'
 } )
 
-
+const Pessoa = require('./pessoas.js')(sequelize, Sequelize); 
 
 sequelize.sync().then(() => console.log('synced'))
